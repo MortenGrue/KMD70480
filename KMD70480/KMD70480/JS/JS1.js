81c3d1f6df2f87ca4c131b7d82d55d555c236934
Morten Grue <mos@kmd.dk>
2016-07-05 10:49:28 -0700

    Randum stuff on page

---
+++ KMD70480/KMD70480/JS/JS1.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('#Button1').click(function () {
+        $('#FirstDiv').append("<p>You clicked the button</p><br />");
+    });
+
+    $('#Text1').click(function () {
+        $('#FirstDiv').append("<p>You clicked the Text</p><br />");
+    });
+});
