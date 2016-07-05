ebdaa9127281d28e7ebd5dd340d3f1ad61e05f81
HHB <hhb@kmd.dk>
2016-07-05 17:57:49 +0200

    ajax initial

---
+++ KMD70480/KMD70480/HHB_AJAX/Ajax.js
@@ -0,0 +1,19 @@
+﻿document.getElementById("txtMessage").focus();
+
+document.getElementById("txtMessage")
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            document.getElementById("btnSubmit").click();
+        }
+    });
+
+
+function StartSync() {
+    var message = document.getElementById("txtMessage").value;
+    console.log("Getting the value");
+
+
+
+    document.getElementById("txtMessage").focus();
+}
