f6d2f7aad2ae505c28174b343743bd92c8ace16e
HHB <hhb@kmd.dk>
2016-07-05 17:34:31 +0200

    endnu mere klar til merge

--- KMD70480/KMD70480/HHB_WebWorker/WebWorker.js
+++ KMD70480/KMD70480/HHB_WebWorker/WebWorker.js
@@ -1,5 +1,8 @@
 ﻿onmessage = function (e) {
     console.log("Message received");
     var d = new Date();
-    postMessage(d.getHours() + ":"+d.getMinutes()+ ":"+d.getSeconds()+": " + e.data);
+    postMessage(d.getHours() +
+        ":" + d.getMinutes() +
+        ":" + d.getSeconds() +
+        ": " + e.data);
 };
