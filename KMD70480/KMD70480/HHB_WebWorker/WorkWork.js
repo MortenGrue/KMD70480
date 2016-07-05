a3a3b16fa2fbfa69b99d73dbf88af969dfa8edbd
HHB <hhb@kmd.dk>
2016-07-05 17:30:51 +0200

    klar til merge

---
+++ KMD70480/KMD70480/HHB_WebWorker/WorkWork.js
@@ -0,0 +1,32 @@
+﻿document.getElementById("txtMessage").focus();
+
+console.log("creating worker..");
+var messageWorker = new Worker("../HHB_WebWorker/WebWorker.js");
+console.log("worker created");
+
+document.getElementById("txtMessage")
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            document.getElementById("btnSubmit").click();
+        }
+    });
+
+messageWorker.onmessage = function (event) {
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(event.data));
+    ul.appendChild(li);
+}
+
+function StartWorker() {
+    console.log("submitMessage method called");
+    var message = document.getElementById("txtMessage").value;
+    console.log("pre-message: " + message);
+    console.log("Posting..");
+    messageWorker.postMessage(message);
+    console.log("posting done.");
+    document.getElementById("txtMessage").focus();
+
+
+}
