33167e3c0911e5c3d35663301a3a868043b51e93
Morten Grue <mos@kmd.dk>
2016-07-07 00:31:29 +0200

    CORS Demo

---
+++ KMD70480/KMD70480/MOS_CORSDemo/JS/CORSDemo.js
@@ -0,0 +1,32 @@
+﻿var invocation = new XMLHttpRequest();
+
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    
+    return xhr;
+}
+
+// Make the actual CORS request.
+function makeCorsRequest() {
+    var time = new Date().getTime();
+    var url = $('#CorsURI').val(); // 'https://cors-test.appspot.com/test'
+
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+
+    xhr.onload = function () {
+        var text = xhr.responseText;
+        time = new Date().getTime() - time;
+        $('#CorsResponsTester').html("Response From: " + url + "<br />With context: " + text + "<br />It took: " + time + "ms" );
+    };
+
+    xhr.onerror = function () {
+        alert('Error making the request.');
+    };
+
+    xhr.send();
+};
