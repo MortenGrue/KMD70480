c022c948a5f3b9f2a694ea58f6b110a23ee4bfa2
Morten Grue <mos@kmd.dk>
2016-07-05 17:20:23 -0700

    Refactored

--- KMD70480/KMD70480/WebWorker/JS/webWorkerDemo.js
+++ KMD70480/KMD70480/WebWorker/JS/webWorkerDemo.js
@@ -3,13 +3,14 @@
 startWorker();
 
 w.onmessage = function (e) {
-    $('#FirstDiv').append(" " + e.data);
+    $('#FirstDiv').append("<br /> " + e.data);
     console.log('Message received from worker');
 
 }
 
-function startWorker() {    
-    w.postMessage("Info to Worker");
+function startWorker() {
+    var s = $('#TextBox1').val();
+    w.postMessage("Info to Worker: " + s);
     console.log('Message posted to worker');
 };
 
