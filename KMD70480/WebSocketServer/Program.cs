86eb3f9ef35b96fd8a6605e3c651da4a350ad91f
Morten Grue <mos@kmd.dk>
2016-07-07 12:01:57 +0200

    Adding WebSocket Web Server

---
+++ KMD70480/WebSocketServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console running!");
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
