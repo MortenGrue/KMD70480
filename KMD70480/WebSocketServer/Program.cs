01641cbc528d4d5ca2bb4694dee9e06031e208df
Morten Grue <mos@kmd.dk>
2016-07-07 13:00:37 +0200

    Web Socket Server v1

--- KMD70480/WebSocketServer/Program.cs
+++ KMD70480/WebSocketServer/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
 
 namespace WebSocketServer
 {
@@ -11,12 +15,36 @@ namespace WebSocketServer
         static void Main(string[] args)
         {
             Console.WriteLine("Console running!");
+            IPAddress ipAddress = Dns.GetHostAddresses("localhost")[1];
 
+            Console.WriteLine("Ipaddress: " + ipAddress);
+            try
+            {
+                TcpListener listner = new TcpListener(ipAddress, 1337);
+                listner.Start();
 
+                TcpClient client = listner.AcceptTcpClient();
+                Console.WriteLine("Connected");
+                client.Client.Send(GetDataStream("Hello for Server!"));
 
-
+                while (client.Connected)
+                {
+                    Random r = new Random();
+                    int i = r.Next(5);
+                    Thread.Sleep(i);
+                    client.Client.Send(GetDataStream("I just wited for: " + i + "Sec. - Are you still there?"));
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Ups...");
+            }
             Console.ReadLine();
+        }
 
+        private static byte[] GetDataStream(string s)
+        {
+            return Convert.FromBase64String(s);
         }
     }
 }
