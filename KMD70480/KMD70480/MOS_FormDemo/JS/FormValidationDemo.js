7b3b4823c1353aacc96ece2c71221ccdc15b68d4
Morten Grue <mos@kmd.dk>
2016-07-07 11:30:45 +0200

    Form Demo

---
+++ KMD70480/KMD70480/MOS_FormDemo/JS/FormValidationDemo.js
@@ -0,0 +1,40 @@
+﻿function ValidateTextBox1() {
+    var textBox = $('#textBox1');
+    var regEx = /\d{3}\w{4}/;
+    if (textBox.val().match(regEx)) {
+        textBox.removeClass("error");
+        textBox.addClass("good");
+        return true;
+    } else {
+        textBox.removeClass("good");
+        textBox.addClass("error");
+        return false;
+    }
+};
+
+
+function ValidateTextBox2() {
+    var textBox = $('#textBox2');
+    var regEx = /\d{3}\w{4}/;
+    if (textBox.val().match(regEx)) {
+        textBox.removeClass("error");
+        textBox.addClass("good");
+        return true;
+    } else {
+        textBox.removeClass("good");
+        textBox.addClass("error");
+        return false;
+    }
+};
+
+function Validate() {
+    var res;
+    res = ValidateTextBox1();
+    res = ValidateTextBox2() && res;
+
+    return res;
+};
+
+function GetId() {
+    return $('#IdField').val();
+};
