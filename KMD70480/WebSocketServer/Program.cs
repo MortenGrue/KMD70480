cc655e618a27d3da4707f9f40be3a51c1ea7c839
Morten Grue <mos@kmd.dk>
2016-07-08 01:05:40 +0200

    Merge MOS and HHB

--- KMD70480/WebSocketServer/Program.cs
+++ KMD70480/WebSocketServer/Program.cs
@@ -7,6 +7,7 @@ using System.Net.Sockets;
 using System.Net;
 using System.IO;
 using System.Threading;
+using System.Security.Cryptography;
 
 namespace WebSocketServer
 {
@@ -17,41 +18,87 @@ namespace WebSocketServer
             Console.WriteLine("Console running!");
             IPAddress ipAddress = Dns.GetHostAddresses("localhost")[1];
             Random r = new Random();
-
+            
             Console.WriteLine("Ipaddress: " + ipAddress);
+
+            string s = string.Empty;
+            string key = string.Empty;
+            var dict = new Dictionary<string,string>();
+            SHA1 sha = new SHA1CryptoServiceProvider();
             try
             {
                 TcpListener listner = new TcpListener(ipAddress, 1337);
                 listner.Start();
 
                 TcpClient client = listner.AcceptTcpClient();
-                Console.WriteLine("Connected");
-                client.Client.Send(GetDataStream("Hello for Server!"));
 
-                while (client.Connected)
+                Console.WriteLine("Connected");
+                
+                
+                using (var writer = new StreamWriter(client.GetStream()))
+                using (var reader = new StreamReader(client.GetStream()))
                 {
-                    if(client.GetStream().DataAvailable)
+                    Thread.Sleep(1000);
+
+                    do
                     {
-                        byte[] b = new byte[1000];
-                        client.GetStream().Read(b, 0, 1000);
-                        Console.WriteLine("Read: " + System.Text.Encoding.Default.GetString(b) + " From client") ;
+                        s = reader.ReadLine();
+                        Console.WriteLine("Read: " + s);
+
+                        if (s.Contains("Sec-WebSocket-Key:"))
+                        {
+                            Console.WriteLine(s.Substring(s.IndexOf(':') + 2));
+                            key = s.Substring(s.IndexOf(':') + 2);
+                            sha.ComputeHash(GetDataStream(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"));
+                        }
+
+                    } while (!string.IsNullOrEmpty(s));
+
+                    Console.WriteLine("Sha1: " + Convert.ToBase64String(sha.Hash));
+
+                    writer.WriteLine("HTTP/1.1 101 Web Socket Protocol Handshake");
+                    writer.WriteLine("Upgrade: WebSocket");
+                    writer.WriteLine("Connection: Upgrade");
+                    writer.WriteLine("Sec-WebSocket-Accept: " + Convert.ToBase64String(sha.Hash));
+                    writer.WriteLine("WebSocket-Origin: http://localhost:1337");
+                    writer.WriteLine("WebSocket-Location:  ws://localhost:1337/");
+                    writer.WriteLine("");
+
+                    writer.Flush();
+
+
+                    while (client.Connected && s != "quit")
+                    {
+                        Console.WriteLine(client.Connected);
+                        s = Console.ReadLine();
+                        //   Console.WriteLine("Sent: "+ s +" to the client.)");
+                        //writer.WriteLine(new byte[] { (byte)WrapperBytes.Start }, 1, 0); // start with a 0x00
+
+                        //writer.Write(((char)WrapperBytes.Start)); // start with a 0x00
+                        writer.Write(GetDataStream(s));
+                        writer.Write(((char)WrapperBytes.End)); // end with a 0xFF
+                        writer.Flush();
+
+                        //client.Client.Send(new byte[] { (byte)WrapperBytes.End }, 1, 0); // end with a 0xFF
                     }
-                    string s = Console.ReadLine();
-                    Console.WriteLine("Sent: \""+ s +"\" to the client.)");
-                    client.Client.Send(GetDataStream(s));
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.StackTrace);
                 Console.WriteLine("Ups...");
+                throw;
             }
+            Console.WriteLine("Closes press enter...");
             Console.ReadLine();
         }
 
         private static byte[] GetDataStream(string s)
         {
-            return Encoding.ASCII.GetBytes(s);
+            return Encoding.UTF8.GetBytes(s);
         }
+
+
+        private enum WrapperBytes : byte { Start = 0, End = 255 };
     }
 }
